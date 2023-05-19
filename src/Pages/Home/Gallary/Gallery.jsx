2fa9b3938d3830af15529d041c85801dd7b96827
Haa-meem <haameemalhamra@gmail.com>
2023-05-20 03:01:44 +0600

    Gallery added

---
+++ src/Pages/Home/Gallary/Gallery.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+const Gallery = () => {
+    const [gallery, setGallery] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setGallery(data))
+    }, [])
+    return (
+        <div className='my-2'>
+            <h1 className='text-center font-bold text-5xl mb-3'>Gallery</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-7 gap-4 bg-orange-300">
+                {
+                    gallery.map(picture =>
+                        <div key={picture.id}>
+                            <img className="w-full h-full rounded-full p-3" src={picture.image} />
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Gallery;
